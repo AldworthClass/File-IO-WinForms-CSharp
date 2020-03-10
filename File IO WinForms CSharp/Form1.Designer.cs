79e39e71d35d94effb18776e48fa71ad2990ae54
Steve Aldworth <s.aldworth@tvdsb.ca>
2020-03-10 09:45:06 -0400

    Add project files.

---
+++ File IO WinForms CSharp/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace File_IO_WinForms_CSharp
+{
+    partial class FormReadWrite
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormReadWrite));
+            this.lstMovies = new System.Windows.Forms.ListBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtAddWords = new System.Windows.Forms.TextBox();
+            this.btnAddWord = new System.Windows.Forms.Button();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstMovies
+            // 
+            this.lstMovies.FormattingEnabled = true;
+            this.lstMovies.Location = new System.Drawing.Point(91, 12);
+            this.lstMovies.Name = "lstMovies";
+            this.lstMovies.Size = new System.Drawing.Size(120, 121);
+            this.lstMovies.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(10, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            // 
+            // txtAddWords
+            // 
+            this.txtAddWords.Location = new System.Drawing.Point(10, 170);
+            this.txtAddWords.Name = "txtAddWords";
+            this.txtAddWords.Size = new System.Drawing.Size(201, 20);
+            this.txtAddWords.TabIndex = 2;
+            // 
+            // btnAddWord
+            // 
+            this.btnAddWord.Location = new System.Drawing.Point(10, 139);
+            this.btnAddWord.Name = "btnAddWord";
+            this.btnAddWord.Size = new System.Drawing.Size(75, 25);
+            this.btnAddWord.TabIndex = 3;
+            this.btnAddWord.Text = "Add";
+            this.btnAddWord.UseVisualStyleBackColor = true;
+            this.btnAddWord.Click += new System.EventHandler(this.btnAddWord_Click);
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(104, 145);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(90, 13);
+            this.lblInstructions.TabIndex = 4;
+            this.lblInstructions.Text = "Enter Movie Title:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(10, 46);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(10, 78);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 6;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(10, 108);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 25);
+            this.btnRemove.TabIndex = 7;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(227, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 178);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormReadWrite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 205);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.btnAddWord);
+            this.Controls.Add(this.txtAddWords);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.lstMovies);
+            this.Name = "FormReadWrite";
+            this.Text = "Reading and Writing Files";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstMovies;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtAddWords;
+        private System.Windows.Forms.Button btnAddWord;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ File IO WinForms CSharp/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_IO_WinForms_CSharp
+{
+    public partial class FormReadWrite : Form
+    {
+        List<string> movies = new List<string>();
+        public FormReadWrite()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+     
+
+        private void btnAddWord_Click(object sender, EventArgs e)
+        {
+            if (movies.Contains(txtAddWords.Text.Trim()))
+                MessageBox.Show("Movie already in list");
+            else if (txtAddWords.Text.Trim().Length == 0)
+                MessageBox.Show("Please enter a movie name");
+            else
+            {
+                movies.Add(txtAddWords.Text.Trim());
+                txtAddWords.Text = "";
+                lstMovies.DataSource = null;
+                lstMovies.DataSource = movies;
+            }
+                
+
+        }
+    }
+}
